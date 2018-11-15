33f070226f601166271fa84af3777031f38bd6bc
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2018-11-15 17:10:30 +0300

    +

--- scripts/convert_images.py
+++ scripts/convert_images.py
@@ -11,14 +11,22 @@ import colored_traceback.auto
 parser = argparse.ArgumentParser(conflict_handler='resolve')
 parser.add = parser.add_argument
 
+parser.add('--img_list',  type=str, help='', default='')
+# then saved to a path/resized/
+# OR
 parser.add('--in_dir',  type=str, help='', default='')
 parser.add('--out_dir',  type=str, default='', help='')
+
 
 parser.add('--out_ext',    type=str, default='', help='')
 parser.add('--out_jpg_q',  type=int, default=99,  help='')
 
 parser.add('--out_img_width',   type=int, default=0,  help='')
 parser.add('--out_img_height',  type=int, default=0,  help='')
+
+parser.add('--save_aspect_ratio',  action='store_true')
+parser.add('--max_dim',  type=int, default=0,  help='')
+
 
 parser.add('--overwrite',  action='store_true')
 
@@ -30,7 +38,25 @@ parser.add('--num_workers',  type=int, default=4, help='')
 args = parser.parse_args()
 
 
-img_exts = ['jpg', 'png', 'jpeg', 'tiff', 'bmp']
+def image_resize(image, specific_size = None, max_dim = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    assert not (specific_size is None and max_dim is None)
+
+    if specific_size is not None:
+        height, width = specific_size
+    else:
+
+        max_dim_ = float(max(h, w))
+
+        height, width = int(round(h / vmax_dim_ * max_dim)), int(round(w / vmax_dim_ * max_dim))  
+      
+    resized = cv2.resize(image, (height, width), interpolation = inter)
+
+    return resized
+
+
 
 def convert(src, dst, args):
     if args.overwrite or not os.path.exists(dst):
@@ -38,17 +64,26 @@ def convert(src, dst, args):
         img = cv2.imread(src, -1)
 
         if args.out_img_width > 0 and args.out_img_height > 0:
-            img = cv2.resize(img, (args.out_img_height, args.out_img_width), cv2.INTER_CUBIC) 
 
-        cv2.imwrite(dst, img, [int(cv2.IMWRITE_JPEG_QUALITY), args.out_jpg_q])
+        if args.save_aspect_ratio:
+            img = image_resize(img, max_dim=args.max_dim)
+        else:
+            img = image_resize(img, specific_size=(args.out_img_height, args.out_img_width)) 
+
+
+        if args.out_ext == 'png':
+            cv2.imwrite(dst, img,  [cv2.IMWRITE_PNG_COMPRESSION, 9])
+        elif args.out_ext == 'jpg':
+            cv2.imwrite(dst, img, [int(cv2.IMWRITE_JPEG_QUALITY), args.out_jpg_q])            
     
 def basename_no_ext(x):
     return '.'.join(os.path.basename(x).split('.')[:-1])
 
 os.makedirs(args.out_dir, exist_ok = True)
 
+img_exts = ['jpg', 'png', 'jpeg', 'tiff', 'bmp']
 in_paths = sum([glob(f'{args.in_dir}/*.{x}') for x in img_exts] + [glob(f'{args.in_dir}/*.{x.upper()}') for x in img_exts], [])
-out_paths = [f"{args.out_dir}/{basename_no_ext(x) + f'.{args.out_ext}'}" for x in in_paths]
+out_paths = [f"{args.out_dir}/{os.path.basename(x) + f'.{args.out_ext}'}" for x in in_paths]
 
 print(f'Number of input images: {len(in_paths)}')
 
