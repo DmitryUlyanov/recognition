7016353d21bd74d1ccccbafb0dece03f91bdc5b4
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2018-11-25 23:35:38 +0300

    +

--- scripts/preds_to_submission.py
+++ scripts/preds_to_submission.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 import json
 import pickle
-
+from tqdm import tqdm
 from glob import glob
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -24,34 +24,38 @@ parser = argparse.ArgumentParser(conflict_handler='resolve')
 parser.add = parser.add_argument
 
 parser.add('--encoding', type=str,    default="", help='')
-
-parser.add('--preds_path', type=str, default="", help='')
-
+parser.add('--preds_dir', type=str, default="", help='')
 parser.add('--save_path', type=str, default="", help='')
-
+parser.add('--sample_submission', type=str, default="", help='')
 
 args = parser.parse_args()
 
+
 with open(f'{args.encoding}', 'r') as fp:
     encoding = json.load(fp)
+mp = {i: v for i, v in enumerate(encoding)}
+print(mp)
 
-with open(f'{args.preds_path}', 'rb') as fp:
-    d = pickle.load(fp)
+sample_submission = pd.read_csv(args.sample_submission)
 
-print(encoding)
+
+
+# preds_dirs = ['../data/experiments/11-22,13:24;config_name:resnet101_stage3']
+
+files = glob(f'{args.preds_dir}/*.npz')
 
 preds = []
-for path, preds_obj in d.items():    
-    preds.append([np.argmax(c) for c in preds_obj])        
-
-df = pd.DataFrame(preds, columns=['Superhero'])
-df.Superhero = df.Superhero.astype(str).map(encoding)
-df['filename'] = [os.path.basename(x)[:-4] for x in d.keys()] 
-
-df['rr'] = df.filename.apply(lambda x: int(x.split('_')[-1]))
-df = df.sort_values('rr')
-
-df = df.drop('rr', axis=1)
-
-
-df[['filename', 'Superhero']].to_csv(f"{args.save_path}", index=False)
+for i, p in enumerate(tqdm(files)):
+    loaded = np.load(p)
+    pred = loaded['output'][0]
+    pred_classes = np.argsort(pred, 1)[:, -3:][:, ::-1]
+    preds.append(np.hstack([ loaded['names'][:, None], pred_classes] ))
+    
+    
+ans = pd.DataFrame(np.vstack(preds), columns=['key_id', 1, 2, 3])
+# print(ans)
+ans['word'] =  ans[1].map(mp).str.replace(' ', '_') + ' ' + ans[2].map(mp).str.replace(' ', '_') + ' ' + ans[3].map(mp).str.replace(' ', '_')
+print(ans['word'])
+# word.replace(' ', '_')
+ans = ans.drop([1, 2, 3], axis=1)
+ans.to_csv(args.save_path + '.gz', compression='gzip', index=False)
