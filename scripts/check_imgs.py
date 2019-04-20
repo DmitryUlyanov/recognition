55c3b3110367e1ce105ede5ee316ed341709afdb
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2019-04-20 20:24:27 +0300

    +

--- scripts/check_imgs.py
+++ scripts/check_imgs.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(conflict_handler='resolve')
 parser.add = parser.add_argument
 
 parser.add('--in_dir',  type=str, help='', default='')
+parser.add('--in_txt',  type=str, help='', default='')
 parser.add('--out_txt',  type=str, help='', default='')
 
 parser.add('--num_workers',  type=int, default=4, help='')
@@ -26,20 +27,20 @@ def check(src):
 
     if img is None:
         print(src)
-        return src
+        return False
     else:
-        return None
+        return True
 
 
-# def basename_no_ext(x):
-#     return '.'.join(os.path.basename(x).split('.')[:-1])
-
-
-# os.makedirs(args.out_dir, exist_ok = True)
-
-in_paths = sum([glob(f'{args.in_dir}/*.{x}') for x in img_exts] + [glob(f'{args.in_dir}/*.{x.upper()}') for x in img_exts], [])
+if args.in_txt == '':
+    in_paths = sum([glob(f'{args.in_dir}/*.{x}') for x in img_exts] + [glob(f'{args.in_dir}/*.{x.upper()}') for x in img_exts], [])
+else:
+    in_paths = np.loadtxt(args.in_txt, dtype=str)
 
 print(f'Number of input images: {len(in_paths)}')
 
 out = Parallel(n_jobs=args.num_workers, verbose=0)(delayed(check)(src) for src in tqdm(in_paths))
-np.savetxt(args.out_txt, [x for x in out if x is not None])
+
+
+np.savetxt(args.out_txt + '_bad', [path for is_ok, path in zip(out, in_paths) if not is_ok], fmt='%s')
+np.savetxt(args.out_txt + '_good', [path for is_ok, path in zip(out, in_paths) if is_ok], fmt='%s')
